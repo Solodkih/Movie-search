9e91480eef50494f1705a632237eb381cda4b6de
Solodkih <solodkih_v_p@mail.ru>
2022-05-22 01:40:59 +0600

    create seach

--- src/scenes/mainScene/navigation/search/index.js
+++ src/scenes/mainScene/navigation/search/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import './search.scss';
+import imgSeach from '../../../../icon/Search.png';
+import imgFilter from '../../../../icon/Filter.png';
 
 export default function Seach() {
-  return <input className="seach" type="text" size="30" placeholder="Seach" />;
+  return (
+    <div className="seach">
+      <input
+        className="seach__button-seach"
+        type="image"
+        src={imgSeach}
+        name="submit"
+        alt="отправить"
+      />
+      <input className="seach__input" type="text" placeholder="Seach" />
+      <input
+        className="seach__button-filter"
+        type="image"
+        src={imgFilter}
+        name="submit"
+        alt="отправить"
+      />
+    </div>
+  );
 }

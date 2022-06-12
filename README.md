6d70a2ab50c72075bb4c344654eba3c422eac5ab
Solodkih <solodkih_v_p@mail.ru>
2022-06-13 00:05:55 +0600

    change name repo and logo

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# movie-search
+# books-search
--- src/scenes/sideBarLeft/logo/index.js
+++ src/scenes/sideBarLeft/logo/index.js
@@ -6,7 +6,7 @@ import './logo.scss';
 export default function Logo({ className }) {
   return (
     <div className={`${className} logo`}>
-      <div className="logo__title">MOVIES</div>
+      <div className="logo__title">BOOKS</div>
       <div className="logo__subtitle">SEARCH</div>
     </div>
   );

c9e8cd1bd7f74b202970fbf637f66fef7c7d0ca3
Solodkih <solodkih_v_p@mail.ru>
2022-08-24 23:39:30 +0600

    create routing and seach params

--- src/scenes/mainScene/searchList/index.js
+++ src/scenes/mainScene/searchList/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useSearchParams } from 'react-router-dom';
 
 export default function SearchList({ className = '' }) {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    async function get() {
+      let params = {
+        query: searchParams.get('query'),
+        title: searchParams.get('title'),
+        author: searchParams.get('author'),
+        subject: searchParams.get('subject'),
+        place: searchParams.get('place'),
+        person: searchParams.get('person'),
+        language: searchParams.get('language'),
+      };
+      console.log(params);
+    }
+    get();
+  }, []);
+
   return <div className={`${className}`}>SearchList</div>;
 }
 

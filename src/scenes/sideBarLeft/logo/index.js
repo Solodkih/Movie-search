e04d31155447cf93951a9d42278aca65220cdc74
Solodkih <solodkih_v_p@mail.ru>
2022-09-17 16:34:46 +0600

    add onClick main logo navigate "/"

--- src/scenes/sideBarLeft/logo/index.js
+++ src/scenes/sideBarLeft/logo/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 
 import './logo.scss';
 
 export default function Logo({ className }) {
+  const navigate = useNavigate();
+
+  function navigateToMainURL() {
+    navigate('/');
+  }
+
   return (
     <div className={`${className} logo`}>
-      <div className="logo__title">BOOKS</div>
-      <div className="logo__subtitle">SEARCH</div>
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={navigateToMainURL}
+        className="logo__title"
+      >
+        BOOKS
+      </div>
+      <div role="button" tabIndex={0} className="logo__subtitle">
+        SEARCH
+      </div>
     </div>
   );
 }

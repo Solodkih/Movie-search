28d7456fefa3905dc589f04f20b9a1fdaaeecf2d
Solodkih <solodkih_v_p@mail.ru>
2022-12-10 00:50:25 +0600

    - add image not found - change style author-card

--- src/scenes/mainScene/author/index.js
+++ src/scenes/mainScene/author/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { getAuthor } from '../../../util/axios/getBooks';
+import ImageNotFound from '../../../components/imageNotFound';
 import './author.scss';
 
 export default function Author({ className = '' }) {
@@ -19,30 +20,34 @@ export default function Author({ className = '' }) {
   return (
     <div className={`${className} author author__container`}>
       <div className="author-main-block">
-        {author.photos[0] && (
-          <div className="author-main-block__image-block">
+        <div className="author-main-block__image-block">
+          {author.photos.length != 0 && (
             <img
               className="author-main-block__image"
               src={author.photos[0]}
               alt={author.name}
             />
-          </div>
-        )}
-        <div className="author-main-block__name">{author.name}</div>
+          )}
+          {author.photos.length == 0 && <ImageNotFound />}
+        </div>
+
+        <div className="author-main-block__name">
+          <span>{author.name}</span>
+        </div>
         <div className="author-main-block__category">
-          <div className="author-main-block__category-title">Personal name:</div>
+          <div className="author-main-block__category-title"><span>Personal name:</span></div>
           <div className="author-main-block__category-data">
             {author.personalName ? author.personalName : 'unknown'}
           </div>
         </div>
         <div className="author-main-block__category">
-          <div className="author-main-block__category-title">Birth date:</div>
+          <div className="author-main-block__category-title"><span>Birth date:</span></div>
           <div className="author-main-block__category-data">
             {author.birthDate ? author.birthDate : 'unknown'}
           </div>
         </div>
         <div className="author-main-block__category">
-          <div className="author-main-block__category-title">Death date:</div>
+          <div className="author-main-block__category-title"><span>Death date:</span></div>
           <div className="author-main-block__category-data">
             {author.deathDate ? author.deathDate : 'unknown'}
           </div>
@@ -50,7 +55,7 @@ export default function Author({ className = '' }) {
         <div className="author-main-block__bio">{author.bio}</div>
       </div>
       <div className="author__category">
-        <div className="author__category-title">Alternate names:</div>
+        <div className="author__category-title"><span>Alternate names:</span></div>
         <div className="author__category-list">
           {author.alternateNames &&
             author.alternateNames.map((item, i, { length }) => {

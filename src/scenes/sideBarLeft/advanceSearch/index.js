f451f0f5fb1af16c3ff28e4899d2f92df22b5c2d
Solodkih <solodkih_v_p@mail.ru>
2024-01-15 18:20:36 +0600

    delete show/hide advance search

--- src/scenes/sideBarLeft/advanceSearch/index.js
+++ src/scenes/sideBarLeft/advanceSearch/index.js
@@ -9,84 +9,73 @@ export default function AdvanceSearch({
   handleOnClickReset,
   handleOnClickSetValue,
 }) {
-  const [showAdvanceSearch, setShowshowAdvanceSearch] = useState(false);
   const { title, author, subject, place, person, publisher } = params;
 
   return (
     <div className={`${className} menu-side-bar`}>
-      <button
-        type="button"
-        onClick={() => {
-          setShowshowAdvanceSearch(!showAdvanceSearch);
-        }}
-        className="menu-side-bar__logo"
-      >
-        Advance Search
-      </button>
-      {showAdvanceSearch && (
-        <ul className="menu-side-bar__list">
-          <li>
-            <ItemSeach
-              data-name="title"
-              placeholder="e.g Pinocchio"
-              value={title}
-              name="Title"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-          <li>
-            <ItemSeach
-              data-name="author"
-              placeholder="e.g J. K. Rowling"
-              value={author}
-              name="Author"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-          <li>
-            <ItemSeach
-              data-name="subject"
-              placeholder="e.g love"
-              value={subject}
-              name="Subject"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-          <li>
-            <ItemSeach
-              data-name="place"
-              placeholder="e.g Moskow"
-              value={place}
-              name="Place"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-          <li>
-            <ItemSeach
-              data-name="person"
-              placeholder="e.g Hermione"
-              value={person}
-              name="Person"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-          <li>
-            <ItemSeach
-              data-name="publisher"
-              placeholder="e.g HarperCollins"
-              value={publisher}
-              name="Publisher"
-              handleOnClickReset={handleOnClickReset}
-              handleOnClickSetValue={handleOnClickSetValue}
-            />
-          </li>
-        </ul>
-      )}
+      <div className="menu-side-bar__logo">Advance Search</div>
+      <ul className="menu-side-bar__list">
+        <li>
+          <ItemSeach
+            data-name="title"
+            placeholder="e.g Pinocchio"
+            value={title}
+            name="Title"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+        <li>
+          <ItemSeach
+            data-name="author"
+            placeholder="e.g J. K. Rowling"
+            value={author}
+            name="Author"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+        <li>
+          <ItemSeach
+            data-name="subject"
+            placeholder="e.g love"
+            value={subject}
+            name="Subject"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+        <li>
+          <ItemSeach
+            data-name="place"
+            placeholder="e.g Moskow"
+            value={place}
+            name="Place"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+        <li>
+          <ItemSeach
+            data-name="person"
+            placeholder="e.g Hermione"
+            value={person}
+            name="Person"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+        <li>
+          <ItemSeach
+            data-name="publisher"
+            placeholder="e.g HarperCollins"
+            value={publisher}
+            name="Publisher"
+            handleOnClickReset={handleOnClickReset}
+            handleOnClickSetValue={handleOnClickSetValue}
+          />
+        </li>
+      </ul>
     </div>
   );
 }

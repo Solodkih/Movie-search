5b41efca209730828e4a9f00f88c70b3dd014c0e
Solodkih <solodkih_v_p@mail.ru>
2024-01-15 18:10:18 +0600

    - delete background loupe - fixed menu in scene - adaptive design side bar

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -5,6 +5,7 @@ import SideBarLeft from './sideBarLeft';
 import MainScene from './mainScene';
 import ModalWindowImage from './modalWindowImage';
 import { useResize } from '../components/hooks/useResize';
+import Loupe from '../components/loupe';
 
 import './scene.scss';
 
@@ -24,7 +25,7 @@ export default function Main() {
           <SideBarLeft
             showMenu={showMenu}
             setShowMenu={setShowMenu}
-            className="scene__side-bar-left"
+            className={`scene__side-bar-left${showMenu ? '__full-dispay' : ''}`}
           />
         )}
         <button
@@ -33,6 +34,11 @@ export default function Main() {
           }}
           className="scene__btn-show-menu"
         >
+          <div
+            className={`scene__loupe scene__loupe__${showMenu ? 'right' : 'left'}`}
+          >
+            <Loupe />
+          </div>
           <i
             className={`scene__arrow scene__arrow__${showMenu ? 'left' : 'right'}`}
           />
